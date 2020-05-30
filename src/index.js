b46355085200c6f25126b501c0f1b6bc90785313
Dominikus Baur <dominikus.baur@gmail.com>
2020-05-30 22:04:32 +0200

    better script organization

--- src/index.js
+++ src/index.js
@@ -3,10 +3,83 @@ import { createFFmpeg } from '@ffmpeg/ffmpeg';
 function initP5Recorder() {
   let _isRecording = false;
   let _config = {};
+  let _p5RecorderInitialized = false;
 
   let _status = "";
 
-  p5.prototype.startRecording = function () {
+  let recordedBlobs = [];
+
+  const transcode = async (webcamData) => {
+    const name = 'record.webm';
+    console.log('Loading ffmpeg-core.js');
+    await ffmpeg.load();
+    console.log('Start transcoding');
+    await ffmpeg.write(name, webcamData);
+    await ffmpeg.transcode(name,  'output.mp4', `-framerate ${_config.framerate} -vf scale=${_config.width}:${_config.height}`);
+    console.log('Complete transcoding');
+    const data = ffmpeg.read('output.mp4');
+
+    const can2 = document.createElement('video');
+    can2.width = _config.width;
+    can2.height = _config.height;
+    can2.loop = true;
+    var videoURL = URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4'}));
+    document.querySelector('body').appendChild(can2);
+    can2.src = videoURL;
+    can2.play();
+  }
+
+  // set up ffmpeg:
+  const ffmpeg = createFFmpeg({
+    corePath: "/node_modules/@ffmpeg/core/ffmpeg-core.js",
+    log: true,
+    logger: p => {console.log('logger:'); console.log(p)},
+    progress: p => console.log(p)
+  });
+
+  p5.prototype.startRecording = function() {
+
+    if(!_p5RecorderInitialized){
+      // extend p5.js draw loop:
+      const _internalDraw = this._draw;
+
+      // stop regular drawing loop
+      this.noLoop();
+
+      this._draw = async () => {
+        _internalDraw();
+
+        if(_isRecording){
+          // store frame
+          document.querySelector('canvas').toBlob(async (blob) => {
+            // TODO: store directly in virtual filesystem instead of creating array?
+            recordedBlobs.push(blob);
+
+            this._requestAnimId = window.requestAnimationFrame(this._draw);
+          });
+        } else {
+          // check if we had a recording going before:
+          if(recordedBlobs.length > 0){
+            const resu = new Uint8Array(await (new Blob(recordedBlobs)).arrayBuffer());
+            await transcode(resu);
+
+            recordedBlobs = [];
+
+            // regular redraw:
+            this._requestAnimId = window.requestAnimationFrame(this._draw);
+
+          } else {
+            // regular redraw:
+            this._requestAnimId = window.requestAnimationFrame(this._draw);
+          }
+        }
+      }
+
+      this._draw();
+
+      _p5RecorderInitialized = true;
+    }
+
     _isRecording = true;
 
     _config = {
@@ -17,103 +90,12 @@ function initP5Recorder() {
     };
 
     console.log(_config);
-
-    const ffmpeg = createFFmpeg({
-      corePath: "../node_modules/@ffmpeg/core/ffmpeg-core.js",
-      log: true,
-      progress: p => console.log(p)
-    });
-       
-
-    const recordedBlobs = [];
-
-    const transcode = async (webcamData) => {
-      const name = 'record.webm';
-      console.log('Loading ffmpeg-core.js');
-      await ffmpeg.load();
-      console.log('Start transcoding');
-      await ffmpeg.write(name, webcamData);
-      await ffmpeg.transcode(name,  'output.mp4', `-framerate ${_config.framerate} -vf scale=${_config.width}:${_config.height}`);
-      console.log('Complete transcoding');
-      const data = ffmpeg.read('output.mp4');
-
-      const can2 = document.createElement('video');
-      can2.width = _config.width;
-      can2.height = _config.height;
-      can2.loop = true;
-      var videoURL = URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4'}));
-      document.querySelector('body').appendChild(can2);
-      can2.src = videoURL;
-      can2.play();
-
-    }
-
-
-    
-    function handleStop(a){
-      console.log(a);
-      console.log('done');
-      var blob = new Blob(recordedBlobs, {type: 'video/webm'});
-      
-      // download
-      /*const ab = document.createElement('a');
-      document.body.appendChild(ab);
-      const url = window.URL.createObjectURL(blob);
-      ab.href = url;
-      ab.download = 'lol.webm';
-      ab.click();
-      setTimeout(() => {
-        window.URL.revokeObjectURL(url);
-        document.body.removeChild(ab);
-      }, 0)*/
-
-      const can2 = document.createElement('video');
-      can2.width = _config.width;
-      can2.height = _config.height;
-      can2.loop = true;
-      var videoURL = URL.createObjectURL(blob);
-      document.querySelector('body').appendChild(can2);
-    can2.src = videoURL;
-    can2.play();
-    }
-
-    console.log(this._renderer);
-
-    let pixels = new Uint8Array(_config.width * _config.height * Math.pow(_config.pixelDensity, 2));
-
-    // disable p5.js draw loop:
-    const _internalDraw = this._draw;
-
-    let counter = 0;
-
-    this._draw = () => {
-      _internalDraw();
-
-      // store frame
-      const ctx = this._renderer.drawingContext;
-      /*pixels = ctx.getImageData(0, 0, _config.width * _config.pixelDensity, _config.height * _config.pixelDensity);
-      pixels = pixels.data;*/
-
-      document.querySelector('canvas').toBlob(async (blob) => {
-        // TODO: store directly in virtual filesystem instead of creating array?
-        recordedBlobs.push(blob);
-
-        counter++;
-        if(counter < 50){
-          this._requestAnimId = window.requestAnimationFrame(this._draw);
-        } else {
-          const resu = new Uint8Array(await (new Blob(recordedBlobs)).arrayBuffer());
-          transcode(resu);
-        }
-
-      });
-    }
-
-    this.noLoop();
-
   };
 
-  // p5.prototype.registerMethod("post", storeFrame);
+  p5.prototype.stopRecording = function() {
+    _isRecording = false;
+  }
+
 }
 
 initP5Recorder();
