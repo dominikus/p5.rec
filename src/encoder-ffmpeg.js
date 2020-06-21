ea78c3f0f9df739f94c62719988f263bb72b1fca
Dominikus Baur <dominikus.baur@gmail.com>
2020-06-21 14:27:46 +0200

    crf option, cleanups

--- src/encoder-ffmpeg.js
+++ src/encoder-ffmpeg.js
@@ -1,52 +1,67 @@
-
-import { createFFmpeg } from '@ffmpeg/ffmpeg';
+import { createFFmpeg } from "@ffmpeg/ffmpeg";
 
 let _ffmpeg;
 let _config;
 
-export const init = () => {
+export const init = (standalone = false) => {
   // set up ffmpeg:
-  _ffmpeg = createFFmpeg({
-    corePath: "/node_modules/@ffmpeg/core/ffmpeg-core.js",
+  let ffmpegOptions = {
     log: false,
     logger: parseProgress,
-  });
-}
+  };
+
+  if (standalone) {
+    ffmpegOptions.corePath = "/node_modules/@ffmpeg/core/ffmpeg-core.js";
+  } else {
+    ffmpegOptions.corePath =
+      "https://unpkg.com/@ffmpeg/core@v0.7.1/ffmpeg-core.js";
+  }
+
+  _ffmpeg = createFFmpeg(ffmpegOptions);
+};
 
 const parseFpsMessageRE = /frame=\s+(\d+)\s+fps/g;
 const parseProgress = (p) => {
   // @ffmpeg can't detect the input duration, so we can't use the 'process' callback :(
-  if(p && p.message){
+  if (p && p.message) {
     let currentFrame;
-    while((currentFrame = parseFpsMessageRE.exec(p.message)) !== null){
+    while ((currentFrame = parseFpsMessageRE.exec(p.message)) !== null) {
       const progress = +currentFrame[1] / _config.totalFrames;
-      if(!isNaN(progress) && _config.onProgress){
+      if (!isNaN(progress) && _config.onProgress) {
         _config.onProgress(progress);
       }
     }
   }
-}
+};
 
 export const transcode = async (imageData, config) => {
+  console.log("starting");
   _config = config;
 
-  const name = 'record.webm';
-  await _ffmpeg.load();
-  await _ffmpeg.write(name, imageData);
-  //const ffmpegParams = `-i ${name} -r ${_config.framerate} -s ${_config.width}x${_config.height} -preset ${_config.preset} -tune animation -pix_fmt yuv420p output.mp4`;
-  
-  const ffmpegParams = `-r ${_config.framerate} -i ${name} -s ${_config.width}x${_config.height} -preset ${_config.preset} -tune animation -pix_fmt yuv420p output.mp4`;
-  
-  await _ffmpeg.run(ffmpegParams);
-  const data = _ffmpeg.read('output.mp4');
+  const inputName = "input.webm";
+  const outputName = "output.mp4";
 
-  const can2 = document.createElement('video');
+  await _ffmpeg.load();
+  await _ffmpeg.write(inputName, imageData);
+
+  const ffmpegParams = `-r ${_config.framerate} -i ${inputName} -s ${_config.width}x${_config.height} -crf ${_config.crf} -preset ${_config.preset} -tune animation -pix_fmt yuv420p ${outputName}`;
+
+  await _ffmpeg.run(ffmpegParams);
+  const data = _ffmpeg.read(outputName);
+
+  // clean up:
+  await _ffmpeg.remove(inputName);
+  await _ffmpeg.remove(outputName);
+
+  const can2 = document.createElement("video");
   can2.width = _config.width;
   can2.height = _config.height;
   can2.loop = true;
   can2.controls = true;
-  var videoURL = URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4'}));
-  document.querySelector('body').appendChild(can2);
+  var videoURL = URL.createObjectURL(
+    new Blob([data.buffer], { type: "video/mp4" })
+  );
+  document.querySelector("body").appendChild(can2);
   can2.src = videoURL;
   can2.play();
-}
+};
--- src/index.js
+++ src/index.js
@@ -1,32 +1,29 @@
-import {init, transcode} from './encoder-ffmpeg.js';
+import { init, transcode } from "./encoder-ffmpeg.js";
 
 const H264_PRESETS = [
-  'ultrafast',
-  'superfast',
-  'veryfast',
-  'faster',
-  'fast',
-  'medium',
-  'slow',
-  'slower',
-  'veryslow'
+  "ultrafast",
+  "superfast",
+  "veryfast",
+  "faster",
+  "fast",
+  "medium",
+  "slow",
+  "slower",
+  "veryslow",
 ];
 
-function initP5Recorder() {
+function initP5Rec() {
   let _isRecording = false;
   let _config = {};
-  let _p5RecorderInitialized = false;
-
-  let _duration = 0;
+  let _p5RecInitialized = false;
 
   let recordedBlobs = [];
 
-  // set up encoder:
-  init();
+  p5.prototype.startRecording = function (options = {}) {
+    if (!_p5RecInitialized) {
+      // set up encoder:
+      init(options.standalone);
 
-  p5.prototype.startRecording = function(options = {}) {
-
-    if(!_p5RecorderInitialized){
       // extend p5.js draw loop:
       const _internalDraw = this._draw;
 
@@ -36,9 +33,9 @@ function initP5Recorder() {
       this._draw = async () => {
         _internalDraw();
 
-        if(_isRecording){
+        if (_isRecording) {
           // store frame
-          document.querySelector('canvas').toBlob(async (blob) => {
+          document.querySelector("canvas").toBlob(async (blob) => {
             // TODO: store directly in virtual filesystem instead of creating array?
             recordedBlobs.push(blob);
 
@@ -46,39 +43,46 @@ function initP5Recorder() {
           });
         } else {
           // check if we had a recording going before:
-          if(recordedBlobs.length > 0){
+          if (recordedBlobs.length > 0) {
             _config.totalFrames = recordedBlobs.length;
 
             console.log(`got ${_config.totalFrames} frames`);
-            const resu = new Uint8Array(await (new Blob(recordedBlobs)).arrayBuffer());
+            const resu = new Uint8Array(
+              await new Blob(recordedBlobs).arrayBuffer()
+            );
             await transcode(resu, _config);
 
             recordedBlobs = [];
 
             // regular redraw:
             this._requestAnimId = window.requestAnimationFrame(this._draw);
-
           } else {
             // regular redraw:
             this._requestAnimId = window.requestAnimationFrame(this._draw);
           }
         }
-      }
+      };
 
       this._draw();
 
-      _p5RecorderInitialized = true;
+      _p5RecInitialized = true;
     }
 
     _isRecording = true;
 
     // parse options:
-    let { preset, onProgress, onEnd } = options;
-    if(!preset || !H264_PRESETS.includes(preset)){
-      preset = 'slow';
+    let { preset, crf, onProgress, onEnd } = options;
+    if (!preset || !H264_PRESETS.includes(preset)) {
+      preset = "slow";
     }
-    if(!onProgress){
-      onProgress = p => console.log(p);
+    if (!onProgress) {
+      onProgress = (p) => console.log(p);
+    }
+    // crf (Constant Rate Factor) defines video quality and
+    // goes from 0 (maximum quality/size) to 51 (worst quality possible)
+    // rf. https://trac.ffmpeg.org/wiki/Encode/H.264#crf
+    if (typeof crf === "undefined" || isNaN(crf) || crf < 0 || crf > 51) {
+      crf = 18;
     }
 
     _config = {
@@ -86,18 +90,18 @@ function initP5Recorder() {
       height: this.height,
       pixelDensity: this._pixelDensity,
       framerate: this._targetFrameRate,
+      crf,
       preset,
       onProgress,
-      onEnd
+      onEnd,
     };
 
     console.log(_config);
   };
 
-  p5.prototype.stopRecording = function() {
+  p5.prototype.stopRecording = function () {
     _isRecording = false;
-  }
-
+  };
 }
 
-initP5Recorder();
+initP5Rec();
